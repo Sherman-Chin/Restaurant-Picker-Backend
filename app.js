61539783628be09b4d56ed25b7a3b7dda7267768
Sherman <shermanchin33@gmail.com>
2020-08-23 16:53:53 +1200

    Modify such that do not repeatedly request data from Zomato

--- app.js
+++ app.js
@@ -12,77 +12,81 @@ const corsOption = {
 };
 
 // All lists for specific searches
-let cuisines = [];
-let establishments = [];
-let categories = [];
+var cuisines = [];
+var establishments = [];
+var categories = [];
 
 const userKey = require(__dirname + "/apiKey.js");
 
-app.get("/", function (req, res) {
-    //Only request data if not requested before
-    if (cuisines.length === 0) {
-        //Get all Cuisines
-        fetch("https://developers.zomato.com/api/v2.1/cuisines?city_id=70",
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "user-key": userKey
-                }
-            }).then(function (response) {
-                return response.json();
-            }).then(function (data) {
-                cuisines.push(data.cuisines);
-                console.log("Requested cuisine data");
-            });
+function getLists() {
+        //Only request data if not requested before
+            //Get all Cuisines
+            fetch("https://developers.zomato.com/api/v2.1/cuisines?city_id=70",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                        "user-key": userKey
+                    }
+                }).then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                    cuisines = data.cuisines;
+                    console.log("Requested cuisine data");
+                });
+    
+            //Get all Categories
+            fetch("https://developers.zomato.com/api/v2.1/establishments?city_id=70",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                        "user-key": userKey
+                    }
+                }).then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                    establishments = data.establishments;
+                    console.log("Requested establishment data");
+                })
+    
+            fetch("https://developers.zomato.com/api/v2.1/categories",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                        "user-key": userKey
+                    }
+                }).then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                    categories = data.categories;
+                    console.log("Requested category data");
+                })
+        
+    
+}
 
-        //Get all Categories
-        fetch("https://developers.zomato.com/api/v2.1/establishments?city_id=70",
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "user-key": userKey
-                }
-            }).then(function (response) {
-                return response.json();
-            }).then(function (data) {
-                establishments.push(data.establishments);
-                console.log("Requested establishment data");
-            })
-
-        fetch("https://developers.zomato.com/api/v2.1/categories",
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "user-key": userKey
-                }
-            }).then(function (response) {
-                return response.json();
-            }).then(function (data) {
-                categories.push(data.categories);
-                console.log("Requested category data");
-            })
-    }
-
-});
 
 app.get("/:listType", cors(corsOption),function (req, res, next) {
     const list = req.params.listType;
 
+    if (cuisines === undefined || cuisines.length === 0) {
+        getLists();
+    }
+
     if (list === "cuisines") {
         console.log(cuisines);
-        res.send(cuisines[0]);
+        res.send(cuisines);
     } else if (list === "establishments") {
         console.log(establishments);
-        res.send(establishments[0]);
+        res.send(establishments);
     } else if (list === "categories") {
         console.log(categories);
-        res.send(categories[0]);
+        res.send(categories);
     } else {
         res.send("Error in providing list.");
     }
